ccae375a251dac75b797e73e9c78eeaa84f1348e
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2024-08-17 09:34:42 +0300

    added base storagable-spawnable architecture

---
+++ CityBuilder/Assets/Scripts/DataStorage/DataStorageBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using CityBuilder.Interfaces;
+using UnityEngine;
+
+namespace CityBuilder.DataStorage
+{
+    public abstract class DataStorageBase<T> : ScriptableObject where T : IStorageable
+    {
+        [SerializeField] private T[] _storageables;
+
+        public bool TryGetItem(int id, out T item)
+        {
+            foreach (var storageable in _storageables)
+            {
+                if (storageable.ID == id)
+                {
+                    item = storageable;
+                    return true;
+                }
+            }
+
+            item = default;
+            return false;
+        }
+
+        public IEnumerable<T> GetAllStorageables()
+        {
+            foreach (T storageable in _storageables)
+            {
+                yield return storageable;
+            }
+        }
+    }
+}
---
+++ CityBuilder/Assets/Scripts/Interfaces/ISpawnable.cs
@@ -0,0 +1,9 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace CityBuilder.Interfaces
+{
+    public interface ISpawnable
+    {
+        public UniTask<bool> Spawn(int id);
+    }
+}
