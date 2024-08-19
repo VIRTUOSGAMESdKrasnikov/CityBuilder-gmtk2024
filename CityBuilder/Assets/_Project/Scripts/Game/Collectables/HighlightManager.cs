e92e1b41ac3edd039ef343fd4794c87a8cccb18d
Bohdan Tsaplia <me@gishadev.com>
2024-08-19 23:48:51 +0300

    highlight fixes

--- CityBuilder/Assets/_Project/Scripts/Game/Collectables/HighlightManager.cs
+++ CityBuilder/Assets/_Project/Scripts/Game/Collectables/HighlightManager.cs
@@ -7,19 +7,20 @@ namespace CityBuilder.Game.Collectables
 {
     public class HighlightManager : MonoBehaviour
     {
-        [SerializeField] private Collectable[] _levelResources;
-
+        private Collectable[] _levelResources;
         private EventBinding<EnteredBuildingMode> _enteredBuildingModeEvent;
         private EventBinding<LeftBuildingMode> _leftBuildingModeEvent;
 
         private void Awake()
         {
+            FindResources();
+            
             var enteredBuilder = new EventBinding<EnteredBuildingMode>.Builder();
             var leftBuilder = new EventBinding<LeftBuildingMode>.Builder();
-            
+
             _enteredBuildingModeEvent = enteredBuilder.WithAction(OnEnteredBuildingMode).Build();
             _leftBuildingModeEvent = leftBuilder.WithAction(OnLeftBuildingMode).Build();
-            
+
             EventBus<EnteredBuildingMode>.Subscribe(_enteredBuildingModeEvent);
             EventBus<LeftBuildingMode>.Subscribe(_leftBuildingModeEvent);
         }
@@ -27,22 +28,25 @@ namespace CityBuilder.Game.Collectables
         private void OnDestroy()
         {
             EventBus<EnteredBuildingMode>.Unsubscribe(_enteredBuildingModeEvent);
+            EventBus<LeftBuildingMode>.Unsubscribe(_leftBuildingModeEvent);
         }
-        
+
+        // TODO: need to be called each island init.
+        private void FindResources()
+        {
+            _levelResources = FindObjectsOfType<Collectable>();
+        }
+
         private void OnEnteredBuildingMode()
         {
             foreach (var resource in _levelResources)
-            {
                 resource.ChangeView(true);
-            }
         }
-        
+
         private void OnLeftBuildingMode()
         {
             foreach (var resource in _levelResources)
-            {
                 resource.ChangeView(false);
-            }
         }
     }
 }
