69fc10da5603e26d845d9c189aac8af4e5287eb7
Bohdan Tsaplia <me@gishadev.com>
2024-08-20 11:01:59 +0300

    cursor webgl fix

--- CityBuilder/Assets/_Project/Scripts/Game/Ui/CardsVisuals/CardAnimations.cs
+++ CityBuilder/Assets/_Project/Scripts/Game/Ui/CardsVisuals/CardAnimations.cs
@@ -11,7 +11,7 @@ namespace CityBuilder.Game.Ui.CardsVisuals
     public class CardAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [Inject] private IRuntimeDataProvider _runtimeDataProvider;
-        
+
         public void OnPointerEnter(PointerEventData eventData)
         {
             var @event = new MouseOverCardEvent();
@@ -20,18 +20,27 @@ namespace CityBuilder.Game.Ui.CardsVisuals
             transform.DOScale(Vector3.one * 1.2f, 0.5f);
 
             var cursorData = _runtimeDataProvider.CursorStates.CardHoveredCursor;
-            Cursor.SetCursor(cursorData.Cursor, cursorData.Hotspot, CursorMode.Auto);
+            SetCursor(cursorData.Cursor, cursorData.Hotspot);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
             var @event = new MouseLeftCardEvent();
             EventBus<MouseLeftCardEvent>.Publish(@event);
-            
+
             transform.DOScale(Vector3.one, 0.5f);
-            
+
             var cursorData = _runtimeDataProvider.CursorStates.DefaultCursor;
-            Cursor.SetCursor(cursorData.Cursor, cursorData.Hotspot, CursorMode.Auto);
+            SetCursor(cursorData.Cursor, cursorData.Hotspot);
+        }
+
+        private void SetCursor(Texture2D cursorTexture, Vector2 hotSpot)
+        {
+#if UNITY_WEBGL
+            Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.ForceSoftware);
+#else
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+#endif
         }
     }
 }
